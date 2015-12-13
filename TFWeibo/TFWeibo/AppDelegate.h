68167ea130bbbd57e1449a6a73764ddf31b34a5b
darren90 <1005052145@qq.com>
2015-12-13 12:50:41 +0800

    init project

---
+++ TFWeibo/TFWeibo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  TFWeibo
+//
+//  Created by Tengfei on 15/12/13.
+//  Copyright © 2015年 tengfei. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
