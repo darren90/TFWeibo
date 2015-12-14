1eb8cea51b71d4e44b2f97ac97c311230301b85f
darren90 <1005052145@qq.com>
2015-12-14 22:29:52 +0800

    init root controller

---
+++ TFWeibo/TFWeibo/Class/Main/Root/RootTabViewController.h
@@ -0,0 +1,13 @@
+//
+//  RootTabViewController.h
+//  TFWeibo
+//
+//  Created by Tengfei on 15/12/14.
+//  Copyright © 2015年 tengfei. All rights reserved.
+//
+
+#import "RDVTabBarController.h"
+
+@interface RootTabViewController : RDVTabBarController
+
+@end
