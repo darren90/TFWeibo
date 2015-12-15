9f0295d538fc6591489b2d1fa06dc62b5c6c0f6a
darren90 <1005052145@qq.com>
2015-12-15 21:49:59 +0800

    添加新特性界面

---
+++ TFWeibo/Pods/Target Support Files/Pods-TFWeiboTests/Pods-TFWeiboTests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_TFWeiboTests : NSObject
+@end
+@implementation PodsDummy_Pods_TFWeiboTests
+@end
---
+++ TFWeibo/Pods/Target Support Files/Pods-TFWeiboUITests/Pods-TFWeiboUITests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_TFWeiboUITests : NSObject
+@end
+@implementation PodsDummy_Pods_TFWeiboUITests
+@end
