3e99f67b9847f9a1710c270b0ebad5ad9cf5f43e
darren90 <1005052145@qq.com>
2015-12-16 07:27:33 +0800

    add tabar omg

--- TFWeibo/TFWeibo/Class/Main/Root/RootTabViewController.m
+++ TFWeibo/TFWeibo/Class/Main/Root/RootTabViewController.m
@@ -55,16 +55,16 @@
 -(void)initTabBarForController
 {
     UIImage *backgroundImage = [UIImage imageNamed:@"tabbar_background"];
-    NSArray *tabBarItemImages = @[@"project", @"task", @"tweet", @"privatemessage", @"me"];
+    NSArray *tabBarItemImages = @[@"tabbar_home", @"tabbar_message_center", @"tweet", @"tabbar_discover", @"tabbar_profile"];
     NSArray *tabBarItemTitles = @[@"首页", @"消息",@"发微博", @"发现", @"我"];
     
     NSInteger index = 0;
-    for (RDVTabBarItem *item in [[self tabBar] items]) {
+    for (RDVTabBarItem *item in self.tabBar.items) {
         item.titlePositionAdjustment = UIOffsetMake(0, 3);
         [item setBackgroundSelectedImage:backgroundImage withUnselectedImage:backgroundImage];
         UIImage *selectedimage = [UIImage imageNamed:[NSString stringWithFormat:@"%@_selected",
                                                       [tabBarItemImages objectAtIndex:index]]];
-        UIImage *unselectedimage = [UIImage imageNamed:[NSString stringWithFormat:@"%@_normal",
+        UIImage *unselectedimage = [UIImage imageNamed:[NSString stringWithFormat:@"%@",
                                                         [tabBarItemImages objectAtIndex:index]]];
         [item setFinishedSelectedImage:selectedimage withFinishedUnselectedImage:unselectedimage];
         [item setTitle:[tabBarItemTitles objectAtIndex:index]];
