01b512ef63ad6f5b6778a703fb8446b8c2b262b1
darren90 <1005052145@qq.com>
2015-12-16 07:39:09 +0800

    init controller

---
+++ TFWeibo/TFWeibo/Class/Home/Controller/HomeController.h
@@ -0,0 +1,13 @@
+//
+//  HomeController.h
+//  TFWeibo
+//
+//  Created by Tengfei on 15/12/16.
+//  Copyright © 2015年 tengfei. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface HomeController : UITableViewController
+
+@end
---
+++ TFWeibo/TFWeibo/Class/Message/Controller/MessageController.h
@@ -0,0 +1,13 @@
+//
+//  MessageController.h
+//  TFWeibo
+//
+//  Created by Tengfei on 15/12/16.
+//  Copyright © 2015年 tengfei. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface MessageController : UITableViewController
+
+@end
