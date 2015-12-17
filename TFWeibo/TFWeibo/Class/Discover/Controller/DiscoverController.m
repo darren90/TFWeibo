168f34248b028ff37590e4701cf76be9e4f723fb
darren90 <1005052145@qq.com>
2015-12-18 07:44:10 +0800

    modify something

--- TFWeibo/TFWeibo/Class/Discover/Controller/DiscoverController.m
+++ TFWeibo/TFWeibo/Class/Discover/Controller/DiscoverController.m
@@ -31,26 +31,25 @@
 }
 
 #pragma mark - Table view data source
-
-- (NSInteger)numberOfSectionsInTableView:(UITableView *)tableView {
-#warning Incomplete implementation, return the number of sections
-    return 0;
-}
-
 - (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section {
-#warning Incomplete implementation, return the number of rows
-    return 0;
+    return 20;
 }
 
-/*
+
 - (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath {
-    UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:<#@"reuseIdentifier"#> forIndexPath:indexPath];
-    
-    // Configure the cell...
+    //1,创建cell
+    static NSString *ID = @"setting";
+    UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:ID];
+    if(cell == nil){
+        cell = [[UITableViewCell alloc] initWithStyle:UITableViewCellStyleDefault reuseIdentifier:ID ];
+    }
+    //2,设置cell的数据
+    cell.textLabel.text = @"discover-1";
     
     return cell;
 }
-*/
+
+
 
 /*
 // Override to support conditional editing of the table view.
