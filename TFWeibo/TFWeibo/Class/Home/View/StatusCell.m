23db5f1dbabb7d6a5fb960243fc175441b1fc428
darren90 <1005052145@qq.com>
2015-12-19 11:26:08 +0800

    setup status cell

--- TFWeibo/TFWeibo/Class/Home/View/StatusCell.m
+++ TFWeibo/TFWeibo/Class/Home/View/StatusCell.m
@@ -11,20 +11,58 @@
 #import "UITapImageView.h"
 #import "UITTTAttributedLabel.h"
 #import "UICustomCollectionView.h"
+#import "UIIconView.h"
 
 @interface StatusCell()
 @property (assign, nonatomic) BOOL needTopView;
 
-@property (strong, nonatomic) UIView *topView;
-@property (strong, nonatomic) UITapImageView *ownerImgView;
-@property (strong, nonatomic) UIButton *ownerNameBtn;
-@property (strong, nonatomic) UITTTAttributedLabel *contentLabel;
-@property (strong, nonatomic) UILabel *timeLabel, *fromLabel;
-@property (strong, nonatomic) UIButton *likeBtn, *commentBtn, *deleteBtn, *shareBtn;
-@property (strong, nonatomic) UIButton *locaitonBtn;
- @property (strong, nonatomic) UICollectionView *likeUsersView;
-@property (strong, nonatomic) UITableView *commentListView;
-@property (strong, nonatomic) UIImageView *timeClockIconView, *commentOrLikeBeginImgView, *commentOrLikeSplitlineView, *fromPhoneIconView;
+//@property (strong, nonatomic) UIView *topView;
+//@property (strong, nonatomic) UITapImageView *ownerImgView;
+//@property (strong, nonatomic) UIButton *ownerNameBtn;
+//@property (strong, nonatomic) UITTTAttributedLabel *contentLabel;
+//@property (strong, nonatomic) UILabel *timeLabel, *fromLabel;
+//@property (strong, nonatomic) UIButton *likeBtn, *commentBtn, *deleteBtn, *shareBtn;
+//@property (strong, nonatomic) UIButton *locaitonBtn;
+// @property (strong, nonatomic) UICollectionView *likeUsersView;
+//@property (strong, nonatomic) UITableView *commentListView;
+//@property (strong, nonatomic) UIImageView *timeClockIconView, *commentOrLikeBeginImgView, *commentOrLikeSplitlineView, *fromPhoneIconView;
+
+
+/** --------1-原创微博整体--------- */
+/** 原创微博-头像 */
+@property (nonatomic,weak)UIIconView * iconView;
+/** 原创微博-会员图标 */
+@property (nonatomic,weak)UIImageView * vipView;
+/** 原创微博-配图 */
+//@property (nonatomic,weak)WBStatusPhotosView * photosView;
+/** 昵称 */
+@property (nonatomic,weak)UILabel * nameLabel;
+/** 时间 */
+@property (nonatomic,weak)UILabel * timeLabel;
+/** 来源 */
+@property (nonatomic,weak)UILabel * sourceLabel;
+/** 正文 */
+@property (nonatomic,weak)UILabel * contentLabel;
+/** 原创微博整体 */
+@property (nonatomic,weak)UIView * originalView;
+/** --------1-原创微博整体--------- */
+
+
+/** --------2-转发微博整体--------- */
+/** 转发微博整体 */
+@property (nonatomic,weak)UIView * retweetView;
+/** 转发正文 */
+@property (nonatomic,weak)UILabel * retweetContentLabel;
+/** 转发配图 */
+//@property (nonatomic,weak)WBStatusPhotosView * retweetPhotosView;
+/** --------2-转发微博整体--------- */
+
+
+
+/** --------3-工具条--------- */
+//@property (nonatomic,weak)WBToolBar * toolBar;
+
+
 
 @end
 
@@ -39,6 +77,63 @@
     
     return cell;
 }
+-(instancetype)initWithStyle:(UITableViewCellStyle)style reuseIdentifier:(NSString *)reuseIdentifier
+{
+    if (self = [super initWithStyle:style reuseIdentifier:reuseIdentifier]) {
+        //1: 初始化原创微博
+        [self initOriginal];
+    }
+    return self;
+}
+
+-(void)initOriginal
+{
+    //1:底部的整体view
+    UIView *originalView = [[UIView alloc]init];
+    [self.contentView addSubview:originalView];
+    self.originalView = originalView;
+    originalView.backgroundColor = [UIColor whiteColor];
+    
+    //2：头像
+    UIIconView *iconView = [[UIIconView alloc]init];
+    self.iconView = iconView;
+    [self.originalView addSubview:iconView];
+    
+    //3:会员图标
+    UIImageView *vipView = [[UIImageView alloc]init];
+    self.vipView = vipView;
+    vipView.contentMode = UIViewContentModeCenter;
+    
+    //4:原创微博配图
+    
+    //5:昵称
+    UILabel *nameLabel = [[UILabel alloc]init];
+    self.nameLabel = nameLabel;
+    [self.originalView addSubview:nameLabel];
+    nameLabel.font = [UIFont systemFontOfSize:15];
+    
+    //6:时间
+    UILabel *timeLabel = [[UILabel alloc]init];
+    self.timeLabel = timeLabel;
+    [self.originalView addSubview:timeLabel];
+    timeLabel.textColor = [UIColor orangeColor];
+    timeLabel.font = [UIFont systemFontOfSize:12];
+    
+    //7:来源
+    UILabel *sourceLabel = [[UILabel alloc]init];
+    self.sourceLabel = sourceLabel;
+    [self.originalView addSubview:sourceLabel];
+    sourceLabel.font = [UIFont systemFontOfSize:12];
+    
+    //8:正文
+    UILabel * contentLabel = [[UILabel alloc]init];
+    [originalView addSubview:contentLabel];
+    self.contentLabel = contentLabel;
+    contentLabel.font = [UIFont systemFontOfSize:14];;
+    contentLabel.numberOfLines = 0;
+}
+
+
 
 
 -(void)setModel:(Status *)model
