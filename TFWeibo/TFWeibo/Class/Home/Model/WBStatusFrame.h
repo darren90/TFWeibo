fb10c2974a5bee33d1defc1643b528772280bf17
darren90 <1005052145@qq.com>
2015-12-21 22:31:59 +0800

    add WBStatusFrame

---
+++ TFWeibo/TFWeibo/Class/Home/Model/WBStatusFrame.h
@@ -0,0 +1,13 @@
+//
+//  WBStatusFrame.h
+//  TFWeibo
+//
+//  Created by Tengfei on 15/12/21.
+//  Copyright © 2015年 tengfei. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface WBStatusFrame : NSObject
+
+@end
---
+++ TFWeibo/TFWeibo/Class/Home/Model/WBStatusFrame.m
@@ -0,0 +1,13 @@
+//
+//  WBStatusFrame.m
+//  TFWeibo
+//
+//  Created by Tengfei on 15/12/21.
+//  Copyright © 2015年 tengfei. All rights reserved.
+//
+
+#import "WBStatusFrame.h"
+
+@implementation WBStatusFrame
+
+@end
