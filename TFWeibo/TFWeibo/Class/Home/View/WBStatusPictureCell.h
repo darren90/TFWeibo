c6ecbd30633b5a888ed87a464807a8069c4d3b5f
darren90 <1005052145@qq.com>
2015-12-23 01:02:18 +0800

    setup WBStatusFrame photoView

---
+++ TFWeibo/TFWeibo/Class/Home/View/WBStatusPictureCell.h
@@ -0,0 +1,13 @@
+//
+//  WBStatusPictureCell.h
+//  TFWeibo
+//
+//  Created by Tengfei on 15/12/23.
+//  Copyright © 2015年 tengfei. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface WBStatusPictureCell : UICollectionViewCell
+
+@end
---
+++ TFWeibo/TFWeibo/Class/Home/View/WBStatusPictureCell.m
@@ -0,0 +1,23 @@
+//
+//  WBStatusPictureCell.m
+//  TFWeibo
+//
+//  Created by Tengfei on 15/12/23.
+//  Copyright © 2015年 tengfei. All rights reserved.
+//
+
+#import "WBStatusPictureCell.h"
+
+@interface WBStatusPictureCell ()
+
+@property (weak, nonatomic) IBOutlet UIImageView *picView;
+
+@end
+
+@implementation WBStatusPictureCell
+
+- (void)awakeFromNib {
+    // Initialization code
+}
+
+@end
