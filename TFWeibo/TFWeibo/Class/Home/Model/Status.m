19fef56a798bc98fd002d6cf95eb11ecc11acb38
darren90 <1005052145@qq.com>
2015-12-24 21:21:54 +0800

    setup create_at

--- TFWeibo/TFWeibo/Class/Home/Model/Status.m
+++ TFWeibo/TFWeibo/Class/Home/Model/Status.m
@@ -24,4 +24,25 @@
      _source = [NSString stringWithFormat:@"来自%@", [source substringWithRange:range]];
 }
 
+
+    // E:星期几
+    // M:月份
+    // d:几号(这个月的第几天)
+    // H:24小时制的小时
+    // m:分钟
+    // s:秒
+    // y:年
+- (NSString *)created_at
+{
+    NSDateFormatter *fmt = [[NSDateFormatter alloc] init];
+    // 如果是真机调试，转换这种欧美时间，需要设置locale
+    fmt.locale = [[NSLocale alloc] initWithLocaleIdentifier:@"local"];
+
+    fmt.dateFormat = @"EEE MMM dd HH:mm:ss Z yyyy";
+    NSDate *createDate = [fmt dateFromString:_created_at];
+    fmt.dateFormat = @"yyy-MMM-dd";
+    NSString *dateStr = [fmt stringFromDate:createDate];
+    return dateStr;
+}
+
 @end
