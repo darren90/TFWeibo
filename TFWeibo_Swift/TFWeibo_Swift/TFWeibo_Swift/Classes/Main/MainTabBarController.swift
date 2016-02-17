ed17ba9e3b58b29c8c964c2fd2633c0d5b48a065
darren90 <1005052145@qq.com>
2016-02-17 22:44:46 +0800

    add 动态设置tabor

--- TFWeibo_Swift/TFWeibo_Swift/TFWeibo_Swift/Classes/Main/MainTabBarController.swift
+++ TFWeibo_Swift/TFWeibo_Swift/TFWeibo_Swift/Classes/Main/MainTabBarController.swift
@@ -13,17 +13,14 @@ class MainTabBarController: UITabBarController {
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        //设置控制器tabbar的颜色
-        //注意：在iOS7以前如果设置了tintcolor只会是文字变，图片不会变
+        //设置控制器tabbar的颜色,：在iOS7以前如果设置了tintcolor只会是文字变，图片不会变
         tabBar.tintColor = UIColor.orangeColor()
                 
-        addChildViewController(HomeTableViewController(), title: "首页", imageName: "tabbar_home")
-        addChildViewController(MessageTableViewController(), title: "消息", imageName: "tabbar_message_center")
-        addChildViewController(DiscoverTableViewController(), title: "广场", imageName: "tabbar_discover")
-        addChildViewController(MeTableViewController(), title: "我", imageName: "tabbar_profile")
+        addChildViewController("HomeTableViewController", title: "首页", imageName: "tabbar_home")
+        addChildViewController("MessageTableViewController", title: "消息", imageName: "tabbar_message_center")
+        addChildViewController("DiscoverTableViewController", title: "广场", imageName: "tabbar_discover")
+        addChildViewController("MeTableViewController", title: "我", imageName: "tabbar_profile")
 //        addChildViewController(HomeTableViewController(), title: "首页", imageName: "")
-
-        
     }
     
     /**
@@ -33,8 +30,21 @@ class MainTabBarController: UITabBarController {
      :param: title           <#title description#>
      :param: imageName       <#imageName description#>
      */
-    private func addChildViewController(childController: UIViewController,title:String,imageName:String) {
+//    private func addChildViewController(childController: UIViewController,title:String,imageName:String) 
+    private func addChildViewController(childControllerName: String,title:String,imageName:String) {
+//        print(childController)
         //1:创建首页
+        //<TFWeibo_Swift.HomeTableViewController: 0x7fa5cb50d3c0>
+        //命名空间默认情况下是项目的名称，但是命名空间的名称可以修改
+        
+        //获取命名空间
+        let ns = NSBundle.mainBundle().infoDictionary!["CFBundleExecutable"] as! String
+        
+        let cls:AnyClass? = NSClassFromString(ns + "." + childControllerName)
+        
+        let clsVc  = cls as! UIViewController.Type
+        let childController = clsVc.init()
+        
         childController.tabBarItem.image = UIImage(named: imageName)
         childController.tabBarItem.selectedImage = UIImage(named: imageName+"_highlighted")
         childController.title = title
