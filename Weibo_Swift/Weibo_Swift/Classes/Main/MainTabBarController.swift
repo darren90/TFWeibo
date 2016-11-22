7ffbc8d21eb9b2c12576b75f8908408ea22d7967
darren90 <1005052145@qq.com>
2016-11-23 00:24:04 +0800

    add

--- Weibo_Swift/Weibo_Swift/Classes/Main/MainTabBarController.swift
+++ Weibo_Swift/Weibo_Swift/Classes/Main/MainTabBarController.swift
@@ -29,6 +29,17 @@ class MainTabBarController: UITabBarController {
     //方法的重载：方法名相同，---》1：参数类型不同，2：参数的个数不同
     //private 私有的方法，只能在当前文件中访问
     private func addChildViewController(vc: UIViewController ,title:String,imgaeName:String) {
+        //根据字符串获取对应的class，在Swift中不能直接使用
+        
+        //Swift中命名空间的概念
+        //
+        let nameSpage = Bundle.main.infoDictionary!["CFBundleExecutable"] as? String
+        guard let childVc = NSClassFromString("HomeViewController") else {
+            print(HomeViewController())
+            print("没有获取到对应的class:\(nameSpage)")
+            return
+        }
+        
         //创建子控制器
         vc.title = title
         vc.tabBarItem.image = UIImage(named: imgaeName)
