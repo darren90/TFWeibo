bce66fcc28aa067cce2a8f87c65ea3bed319c458
darren90 <1005052145@qq.com>
2016-12-13 00:41:41 +0800

    add Nettools

--- Weibo_Swift/Weibo_Swift/Classes/Tools/NetWorkTools.swift
+++ Weibo_Swift/Weibo_Swift/Classes/Tools/NetWorkTools.swift
@@ -1,34 +1,66 @@
 //
 //  NetWorkTools.swift
-//  Weibo_Swift
+//  AFNSwiftUse
 //
-//  Created by Tengfei on 16/11/28.
+//  Created by Tengfei on 2016/12/8.
 //  Copyright © 2016年 tengfei. All rights reserved.
 //
 
-import UIKit
+import AFNetworking
 
-enum RequestType:String{
+enum RequestType :String {
     case GET = "GET"
     case POST = "POST"
 }
 
-class NetWorkTools: NSObject {
-
-    //单例
-    static let share:NetWorkTools = {
-        let tools = NetWorkTools()
-//        tools.respon
+class NetWorkTools: AFHTTPSessionManager {
+    //let是线程安全的 ，这就是单例的
+    static let shareInstance  : NetWorkTools = {
+        let tools  = NetWorkTools()
+        tools.responseSerializer.acceptableContentTypes?.insert("text/html")
         return tools
     }()
     
-    func request(method : RequestType,urlStr : String,pams:[String : AnyObject] ,finsinsh : (_ result : AnyObject?, _ error : NSError?) -> () ){
+    
+    // MARK:-- 封装请求方法
+    
+    //    func request(methodType:RequestType , urlString:String , params : [String : AnyObject]) {
+    //        if methodType == .GET {
+    //            get(urlString, parameters: params, progress: nil, success: { (task : URLSessionDataTask, result : Any?) in
+    //                print(result)
+    //            }) { (task : URLSessionDataTask?,  error:Error) in
+    //                print(error)
+    //            }
+    //
+    //        }else{
+    //            post(urlString, parameters: params, progress: nil, success: { (task : URLSessionDataTask, result : Any?) in
+    //
+    //            }) { (task : URLSessionDataTask?, error : Error) in
+    //
+    //            }
+    //        }
+    //
+    //    }
+    
+    func request(methodType : RequestType, urlString : String, parameters : [String : AnyObject], finished : @escaping (_ result : Any?, _ error : Error?) -> ()) {
         
-        if method == .GET {
-//                GET()
-        }else{
-        
+        // 1.定义成功的回调闭包
+        let successCallBack = { (task : URLSessionDataTask, result : Any?) -> Void in
+            finished(result, nil)
         }
+        
+        // 2.定义失败的回调闭包
+        let failureCallBack = { (task : URLSessionDataTask?, error : Error) -> Void in
+            finished(nil, error)
+        }
+        
+        // 3.发送网络请求
+        if methodType == .GET {
+            get(urlString, parameters: parameters, progress: nil, success: successCallBack, failure: failureCallBack)
+        } else {
+            post(urlString, parameters: parameters, progress: nil, success: successCallBack, failure: failureCallBack)
+        }
+        
     }
     
 }
